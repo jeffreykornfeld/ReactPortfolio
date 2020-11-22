8c3abba9297893e2d2b505c70e3658c16265110b
Jeffrey Kornfeld <jakornfeld@yahoo.com>
2020-11-21 23:53:06 -0600

    worked on main and portfolio components

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,26 +8,25 @@ export default class Main extends Component {
 
 
     <div class="container">
-        
-        
         <div class="row">
             <div class="col-md-12">
                 <h1>About Me</h1>
             </div>
         
         </div>
-     
         <div class="row">
-            <div class="col-md-6">
+            
                 <img src= {MePic} alt="ActionShot"></img>
-            </div>
+            
+        </div>
+        <div class="textbody">
 
-            <div class="col-md-6">
+            
                 <p> I'm a musician of 20+ years and an asipiring web developer. I'm currently enrolled in a Northwestern University Coding Bootcamp.
-                    Currently experienced with HTML, CSS, Javascript, APIs, OOP, NodeJS, express, MySQL, MVC (handlebars). </p>
+                    Currently experienced with HTML, CSS, Javascript, APIs, OOP, NodeJS, express, MySQL, MVC (handlebars) and React. </p>
                 <br></br>
                 <a href="https://github.com/jeffreykornfeld"> My GitHub</a> 
-            </div>
+            
         </div>
     </div>
 
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,35 +1,57 @@
 import React, { Component } from 'react';
-import "../styles/Main.css"
+import "../styles/Portfolio.css"
+import LetsCelebrate from "../assets/images/LetsCelebrate.png"
+import WeatherDashboard from "../assets/images/WeatherDashboard.png"
+import GamingGladiators from "../assets/images/gaminggladiators.png"
+import WorkdayScheduler from "../assets/images/WorkDayScheduler.png"
+import PasswordGenerator from "../assets/images/PasswordGenerator.png"
+import TemplateEngine from "../assets/images/TemplateEngine.png"
 
 export default class Portfolio extends Component {
     render() {
         return (
 
-
-    <div class="container">
-        
-        
-        <div class="row">
-            <div class="col-md-12">
-                <h1>About Me</h1>
+        <div class="container">
+             <div class="row">
+                <div class="col-md-12">
+                    <h1> Portfolio </h1> 
+                </div>
             </div>
-        
-        </div>
+
+            <div class="row">
+                
+                <div class="col-md-4">
+                    <img src= {LetsCelebrate} alt="LetsCelebrate" class="img-thumbnail"></img>
+                    <a class="nav-link" href="https://github.com/kbaur775/letscelebrate">Let's Celebrate!</a>
+                    
+                </div>
      
-        <div class="row">
-            <div class="col-md-6">
-                <img src="Assets/Images/BandW.jpg" alt="ActionShot"></img>
+                <div class="col-md-4">
+                    <img src={WeatherDashboard} alt="WeatherDashboard" class="img-thumbnail"></img>
+                    <a class="nav-link" href="https://github.com/jeffreykornfeld/WeatherDashboard">Weather Dashboard</a>
+                </div>
+                
+                <div class="col-md-4">
+                    <img src={GamingGladiators} alt="GamingGladiators" class="img-thumbnail"></img>
+                    <a class="nav-link" href="https://github.com/dktrcoco/GamingGladiator">Gaming Gladiator</a>
+                </div>
             </div>
 
-            <div class="col-md-6">
-                <p> I'm a musician of 20+ years and an asipiring web developer. I'm currently enrolled in a Northwestern University Coding Bootcamp.
-                    Currently experienced with HTML, CSS, Javascript, APIs, OOP, NodeJS, express, MySQL, MVC (handlebars). </p>
-                <br></br>
-                <a href="https://github.com/jeffreykornfeld"> My GitHub</a> 
+            <div class="row">
+                <div class="col-md-4">
+                    <img src={WorkdayScheduler} alt="WorkdayScheduler" class="img-thumbnail"></img>
+                    <a class="nav-link" href="https://github.com/jeffreykornfeld/WorkDayScheduler">Workday Scheduler</a>
+                </div>
+                <div class="col-md-4">
+                    <img src={PasswordGenerator} alt="PasswordGenerator" class="img-thumbnail"></img>
+                    <a class="nav-link" href="https://github.com/jeffreykornfeld/PasswordGenerator">Password Generator</a>
+                </div>
+                <div class="col-md-4">
+                    <img src={TemplateEngine} alt="TemplateEngine" class="img-thumbnail"></img>
+                    <a class="nav-link" href="https://github.com/jeffreykornfeld/TemplateEngine_EmployeeSummary">Employee Summary Template Engine</a>
+                </div>
             </div>
         </div>
-    </div>
-
 
         )
     }
