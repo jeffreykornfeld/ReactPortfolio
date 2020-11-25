6e648c6a2410f49cc9dbc413a643139f3cbbd249
Jeffrey Kornfeld <jakornfeld@yahoo.com>
2020-11-25 15:04:53 -0600

    router basename added to App.js

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
-  <div>
-  <Header />
-  <Switch>
-
-    <Route path="/" exact component={About}/>
-    <Route path="/about" component={About}/>
-    <Route path="/repos" component={Repos}/>
-  </Switch>
-  </div>
+  <Router basename={process.env.PUBLIC_URL}>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={About}/>
+        <Route path="/about" component={About}/>
+        <Route path="/repos" component={Repos}/>
+        <Route component={() => (<div>404 Not Found</div>)} />
+      </Switch>
+    </div>
   </Router>
   )
 }
